dfa6d9341184c74dbbf505cd27e1294da73fa8dd
khacnguy <90940136+khacnguy@users.noreply.github.com>
2023-12-25 19:57:28 -0700

    Add files via upload

---
+++ detection_custom.py
@@ -0,0 +1,30 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.saved_model import tag_constants
+from yolov3.yolov4 import Create_Yolo
+from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
+from yolov3.configs import *
+
+image_path   = "./dataset/test/000382481.jpg"
+video_path   = "./IMAGES/test.mp4"
+
+if YOLO_FRAMEWORK == "tf": # TensorFlow detection
+    if YOLO_TYPE == "yolov4":
+        Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
+    if YOLO_TYPE == "yolov3":
+        Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
+    yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
+    
+    yolo.load_weights(YOLO_CUSTOM_WEIGHTS) # use custom weights
+    
+elif YOLO_FRAMEWORK == "trt": # TensorRT detection
+    saved_model_loaded = tf.saved_model.load(YOLO_CUSTOM_WEIGHTS, tags=[tag_constants.SERVING])
+    signature_keys = list(saved_model_loaded.signatures.keys())
+    yolo = saved_model_loaded.signatures['serving_default']
+
+detect_image(yolo, image_path, "./IMAGES/detect1.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
